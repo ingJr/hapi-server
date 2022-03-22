37c26bfeefe70944cac224710c67a73546affc08
ingJr <acmejr@hotmail.com>
2022-03-22 17:47:37 -0500

    cambiar bd

--- databse.js
+++ databse.js
@@ -1,10 +1,14 @@
 'use strict'
 
-const {MongoClient} = require('mongodb')
+//const {MongoClient} = require('mongodb')
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
 var state = { db: null };
 const db = 'rinnDB';
-const url = 'mongodb+srv://root_DB:uMZjGJ6nsxRYR8NV@cluster0.sixcs.mongodb.net/rinnDB'
+//const url = 'mongodb+srv://root_DB:uMZjGJ6nsxRYR8NV@cluster0.sixcs.mongodb.net/rinnDB'
+const url = 'mongodb://root_DB:uMZjGJ6nsxRYR8NV@localhost:27017/rinnDB'
+//const url = 'mongodb://localhost:27017/rinnDB'
 
 const connect =()=>{
     return new Promise((resolve, reject) => {
